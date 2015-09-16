68c87b75bd538f3f5e9930dc54ef549e2ada2e09
Székely Ádám <aszekely@brainsum.com>
2015-09-16 16:17:05 +0200

    minit.cz #2018 | theme-development (form)

--- view/layout.php
+++ view/layout.php
@@ -169,7 +169,7 @@ $view = Brainsum\App::getConfig();
             </div>
         </section>
 
-        <!-- [MAIN.NODE-LIST] -->
+        <!-- [MAIN.NODE-LIST] YEARS -->
 
         <section class="block block-lead content">
             <div class="container">
@@ -207,7 +207,7 @@ $view = Brainsum\App::getConfig();
 
         <!-- [MAIN.NODE-LIST] PLAIN -->
 
-        <section class="block block-lead content">
+        <section class="block block-fade content">
             <div class="container">
                 <h2 class="title">Staňte sa franchisistom</h2>
                 <h4 class="lead">Je to jednoduché</h4>
@@ -220,6 +220,50 @@ $view = Brainsum\App::getConfig();
                     <li class="item">Listaelem #6</li>
                     <li class="item">Listaelem #7</li>
                 </ul>
+            </div>
+        </section>
+
+        <!-- [MAIN.NODE-FORM] -->
+
+        <section class="block block-lead content">
+            <div class="container">
+                <h2 class="title">Získať viac informácií</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut laoreet porttitor risus, mollis vulputate nibh. Proin vel sagittis ex, eu aliquam sapien.</p>
+
+                <form class="form" name="form-info" action="/send">
+                    <div class="item required">
+                        <label class="label" for="info-name">Vaše meni/firma</label>
+                        <input class="input" id="info-name" name="name" type="text" maxlength="255" required=""/>
+                    </div>
+                    <div class="item required">
+                        <label class="label" for="info-address">Zadajte okres</label>
+                        <input class="input" id="info-address" name="address" type="text" maxlength="255" required=""/>
+                    </div>
+                    <div class="item required">
+                        <label class="label" for="info-phone">Kontaktné tel. číslo</label>
+                        <input class="input" id="info-phone" name="phone" type="text" maxlength="255" required=""/>
+                    </div>
+                    <div class="item required">
+                        <label class="label" for="info-phone">E-mail</label>
+                        <input class="input" id="info-email" name="email" type="email" maxlength="255" required=""/>
+                    </div>
+                    <div class="item item-expand">
+                        <label class="label" for="info-message">Stručný popis Vašej prevádzky</label>
+                        <textarea class="input" name="message" id="info-message"></textarea>
+                    </div>
+                    <div class="item item-expand">
+                        <label class="label" for="info-cooperation">Vaša predstava o spolupráci / o aký sortiment máte záujem</label>
+                        <textarea class="input" name="cooperation" id="info-cooperation"></textarea>
+                    </div>
+                    <div class="item item-expand required">
+                        <img class="code" src="about:blank" alt=""/>
+                        <label class="label" for="info-code">Zadajte kód z obrázku</label>
+                        <input class="input" name="code" id="info-code" type="text" autocomplete="off" required=""/>
+                    </div>
+
+                    <input class="btn" type="submit" value="Odoslať"/>
+                    <input type="hidden" name="token" value=""/>
+                </form>
             </div>
         </section>
     </div>
