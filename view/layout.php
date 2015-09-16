7358164ba3f20ab709d7beef666cccac2165f316
Székely Ádám <aszekely@brainsum.com>
2015-09-16 15:11:46 +0200

    minit.cz #2018 | theme-development (lists)

--- view/layout.php
+++ view/layout.php
@@ -168,6 +168,60 @@ $view = Brainsum\App::getConfig();
                 <a class="btn btn-next" href="#">Chem vedieť viac</a>
             </div>
         </section>
+
+        <!-- [MAIN.NODE-LIST] -->
+
+        <section class="block block-lead content">
+            <div class="container">
+                <h2 class="title">Ocenenia</h2>
+                <ul class="list list-year">
+                    <li class="item">
+                        <h5 class="year">2009</h5>
+                        <div class="info">
+                            <ul class="list text">
+                                <li class="item">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                                <li class="item">Ut laoreet porttitor risus, mollis vulputate nibh. Proin vel sagittis ex, eu aliquam sapien.</li>
+                                <li class="item">Vivamus sed venenatis elit.</li>
+                            </ul>
+                            <ul class="list icon">
+                                <li class="item"><img class="logo" src="http://i3.cpcache.com/product/5024365/tile_coaster.jpg?height=100&width=100" alt=""/></li>
+                                <li class="item"><img class="logo" src="http://i3.cpcache.com/product/5024365/tile_coaster.jpg?height=100&width=100" alt=""/></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="item">
+                        <h5 class="year">2008</h5>
+                        <div class="info">
+                            <ul class="list text">
+                                <li class="item">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                                <li class="item">Ut laoreet porttitor risus, mollis vulputate nibh. Proin vel sagittis ex, eu aliquam sapien.</li>
+                            </ul>
+                            <ul class="list icon">
+                                <li class="item"><img class="logo" src="http://i3.cpcache.com/product/5024365/tile_coaster.jpg?height=100&width=100" alt=""/></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+        <!-- [MAIN.NODE-LIST] PLAIN -->
+
+        <section class="block block-lead content">
+            <div class="container">
+                <h2 class="title">Staňte sa franchisistom</h2>
+                <h4 class="lead">Je to jednoduché</h4>
+                <ul class="list list-plain">
+                    <li class="item">Listaelem #1</li>
+                    <li class="item">Listaelem #2</li>
+                    <li class="item">Listaelem #3</li>
+                    <li class="item">Listaelem #4</li>
+                    <li class="item">Listaelem #5</li>
+                    <li class="item">Listaelem #6</li>
+                    <li class="item">Listaelem #7</li>
+                </ul>
+            </div>
+        </section>
     </div>
 </article>
 
