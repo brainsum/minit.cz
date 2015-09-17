5ac27d0f92ce9557bbef35e2995ad9fc32db5ce7
keeteean <keeteean@gmail.com>
2015-09-17 14:33:55 +0200

    philosophy section

---
+++ view/components/philosophy.html.php
@@ -0,0 +1,40 @@
+<!-- [MAIN.NODE-TEXT] CONTENT -->
+
+<section id="philosophy" class="block block-lead content">
+    <div class="container">
+        <h2 class="title">Filozofie</h2>
+        <p>Nabízíme už více jak 50 druhů produktů rozličných chutí, velikostí a tvarů. Ačkoliv jsou různé, řídíme se při výrobě každého z nich stejnou filozofií.</p>
+        <ul class="inline-container container">
+	        <li class="inline xs-4">
+	            <figure class="image-element">
+                    <figcaption class="caption">
+                        <strong>Čerstvost</strong>
+                    </figcaption>
+	                <object data="img/icon-vector-freshness.svg" type="image/svg+xml" alt="Čerstvost" width="42" height="48">
+		                <img src="img/icon-freshness.png" alt="Čerstvost" width="42" height="48"/>
+		            </object>
+                </figure>
+	        </li>
+	        <li class="inline xs-4">
+	            <figure class="image-element">
+                    <figcaption class="caption">
+                        <strong>Stálá kvalita</strong>
+                    </figcaption>
+	                <object data="img/icon-vector-consistent-quality.svg" type="image/svg+xml" alt="Stálá kvalita" width="46" height="48">
+		                <img src="img/icon-consistent-quality.png" alt="Stálá kvalita" width="46" height="48"/>
+		            </object>
+                </figure>
+	        </li>
+	        <li class="inline xs-4">
+	            <figure class="image-element">
+                    <figcaption class="caption">
+                        <strong>Přiměřená cena</strong>
+                    </figcaption>
+	                <object data="img/icon-vector-reasonable-price.svg" type="image/svg+xml" alt="Přiměřená cena" width="48" height="45">
+		                <img src="img/icon-reasonable-price.png" alt="Přiměřená cena" width="48" height="45"/>
+		            </object>
+                </figure>
+	        </li>
+	    </ul>
+    </div>
+</section>
