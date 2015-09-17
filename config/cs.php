fafb1d9de77cabcd9c2ee74400f1682897f5a2a4
keeteean <keeteean@gmail.com>
2015-09-17 17:17:38 +0200

    Franchisee section

--- config/cs.php
+++ config/cs.php
@@ -15,7 +15,7 @@
         ),
         'pro-partnery' => array(
             'title' => 'Pro partnery',
-            'block' => array('splash', 'cooperation', 'list-plain', 'list-year', 'form')
+            'block' => array('splash', 'cooperation', 'franchisee', 'list-year', 'form')
         ),
         'kontakty' => array(
             'title' => 'Kontakty'
---
+++ view/components/franchisee.html.php
@@ -0,0 +1,17 @@
+<!-- [MAIN.NODE-LIST] PLAIN -->
+
+<section class="block block-fade content">
+    <div class="container">
+        <h2 class="title">Staňte se franšízantem</h2>
+        <h4 class="lead">Je to jednoduché</h4>
+        <ul class="list list-plain">
+            <li class="item">Můžete být právnická osoba nebo fyzická osoba s povolením podnikat.</li>
+            <li class="item">Obstaráte si prostor nebo ho budete hledat ve spolupráci s námi.</li>
+            <li class="item">Zabezpečíte všechny náležitosti, tedy pokladnu, váhu, mrazák, personál a podobně.</li>
+            <li class="item">Složíte vratnou kauci.</li>
+            <li class="item">Dodáme vám potřebné vybavení na pečení a na výzdobu prodejny.</li>
+            <li class="item">Náš zástupce vše přijde nainstalovat do prodejny a vyškolí vaše zaměstnance.</li>
+            <li class="item">To je vše! Jste připraven prodat první minitky.</li>
+        </ul>
+    </div>
+</section>
