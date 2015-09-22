d3ec5ed09bf4143735c4c44c56d015d227e2c80d
Krisztian Pinter <kpinter@brainsum.com>
2015-09-22 17:06:53 +0200

    added facebook link

--- view/layout.html.php
+++ view/layout.html.php
@@ -100,7 +100,7 @@ $path = Brainsum\App::getRouter()->getPath();
         <li class="inline col xs-3">
             <h6 class="title">Online komunita</h6>
             <ul class="list">
-                <li class="item"><a class="link" href="#">Facebook</a></li>
+                <li class="item"><a class="link" href="//www.facebook.com/minitkacz">Facebook</a></li>
             </ul>
         </li>
         <li class="inline col xs-3">
