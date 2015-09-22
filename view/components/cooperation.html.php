ba2de46e239d63a725ce14ce6244d1c1146debb9
Krisztian Pinter <kpinter@brainsum.com>
2015-09-22 12:42:54 +0200

    typo fixed in cooperation section

--- view/components/cooperation.html.php
+++ view/components/cooperation.html.php
@@ -6,8 +6,8 @@
         <ul class="list text">
             <li class="item">Budete spolupracovat s důvěryhodným a stabilním partnerem.</li>
             <li class="item">Dodáme vám veškeré marketingové materiály a know-how.</li>
-            <li class="item">K dispozici a stále na blízku vám bude náš obchodní zástupce, který zároveň vyškolí váš personál-</li>
-            <li class="item">Produkty, které budete prodávat, vám vždy dodáme včas a ve stálé kvalitě.</li>            
+            <li class="item">K dispozici a stále na blízku vám bude náš obchodní zástupce, který zároveň vyškolí váš personál.</li>
+            <li class="item">Produkty, které budete prodávat, vám vždy dodáme včas a ve stálé kvalitě.</li>
         </ul>
     </div>
 </section>
