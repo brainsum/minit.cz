81a6bc322d4c7a438085eda4a7cc11b6481c6e14
Krisztian Pinter <kpinter@brainsum.com>
2015-10-02 16:40:54 +0200

    updated 2. slide conent and order

--- view/components/slider.html.php
+++ view/components/slider.html.php
@@ -16,6 +16,18 @@
     <ul class="inline-container view module">
         <li class="inline-container view item">
             <div class="inline">
+                <h3 class="title">Říjnová Rollo akce</h3>
+                <figure class="inline-container container">
+                    <img class="inline image" src="img/fornetti-minit-offer-croissant-poppy-seed.jpg" srcset="img/fornetti-minit-offer-croissant-poppy-seed.jpg 1x, img/hd/fornetti-minit-offer-croissant-poppy-seed.jpg 2x, img/hq/fornetti-minit-offer-croissant-poppy-seed.jpg 3x" alt="Říjnová Rollo akce" width="476" height="460" />
+                    <figcaption class="inline xs-6 text">
+                        <p>Připravili jsme pro skvělou akci. Tři maková a dvě skořicová Rolla pořídíte v průběhu října za dvacetikorunu! Novinku v našem portfoliu výrobků tak můžete ochutnat za výhodnou cenu.</p>
+                        <strong class="last">Akce probíhá na vybraných pobočkách</strong>
+                    </figcaption>
+                </figure>
+            </div>
+        </li>
+        <li class="inline-container view item">
+            <div class="inline">
                 <h3 class="title">MINIT má novou prodejnu v Českých Budějovicích</h3>
                 <figure class="inline-container container">
                     <figcaption class="inline xs-6 text">
@@ -23,18 +35,6 @@
                         <strong class="last">Těšíme se na vaši návštěvu!</strong>
                     </figcaption>
                     <img class="inline image" src="img/fornetti-minit-new-store.jpg" srcset="img/fornetti-minit-new-store.jpg 1x, img/hd/fornetti-minit-new-store.jpg 2x, img/hq/fornetti-minit-new-store.jpg 3x" alt="Fornetti Minit nova prodejna" width="476" height="323" />
-                </figure>
-            </div>
-        </li>
-        <li class="inline-container view item">
-            <div class="inline">
-                <h3 class="title">Vychutnejte si novinku – makové Rollo</h3>
-                <figure class="inline-container container">
-                    <img class="inline image" src="img/fornetti-minit-croissant-poppy-seed.png" srcset="img/fornetti-minit-croissant-poppy-seed.png 1x, img/hd/fornetti-minit-croissant-poppy-seed.png 2x, img/hq/fornetti-minit-croissant-poppy-seed.png 3x" alt="Fornetti Minit Makové Rollo" width="476" height="460" />
-                    <figcaption class="inline xs-6 text">
-                        <p>Mák patří k naší zemi stejně jako špagety k Itálii. Vaše velká poptávka po makových produktech nám vnukla skvělý nápad. Stávajícímu skořicovému Rollu tak přibyl makový bratr!</p>
-                        <strong class="last">Vychutnat si ho můžete již teď na všech prodejnách.</strong>
-                    </figcaption>
                 </figure>
             </div>
         </li>
